a5c3392ea337d9de02a601cb6bf49445fa1b6702
Jeremy Keeshin <jkeesh@gmail.com>
2011-09-23 12:25:49 -0700

    update format time

--- application/qa/templatetags/all_tags.py
+++ application/qa/templatetags/all_tags.py
@@ -12,3 +12,48 @@ def version():
 @register.simple_tag
 def debug_js():
     return "true" if settings.CONSOLE_DEBUG else "false"
+
+
+@register.simple_tag
+def format_time(timestamp = None):
+    """
+    Returns a humanized string representing time difference
+    between now() and the input timestamp.
+
+    The output rounds up to days, hours, minutes, or seconds.
+    4 days 5 hours returns '4 days'
+    0 days 4 hours 3 minutes returns '4 hours', etc...
+    http://djangosnippets.org/snippets/412/
+    """
+    import datetime
+
+    timeDiff = datetime.datetime.now() - timestamp
+    days = timeDiff.days
+    hours = timeDiff.seconds/3600
+    minutes = timeDiff.seconds%3600/60
+    seconds = timeDiff.seconds%3600%60
+
+    str = ""
+    tStr = ""
+    if days > 0:
+        if days == 1:   tStr = "day"
+        else:           tStr = "days"
+        str = str + "%s %s ago" %(days, tStr)
+        return str
+    elif hours > 0:
+        if hours == 1:  tStr = "hour"
+        else:           tStr = "hours"
+        str = str + "%s %s ago" %(hours, tStr)
+        return str
+    elif minutes > 0:
+        if minutes == 1:tStr = "min"
+        else:           tStr = "mins"           
+        str = str + "%s %s ago" %(minutes, tStr)
+        return str
+    elif seconds > 0:
+        if seconds == 1:tStr = "sec"
+        else:           tStr = "secs"
+        str = str + "%s %s ago" %(seconds, tStr)
+        return str
+    else:
+        return "just now"
