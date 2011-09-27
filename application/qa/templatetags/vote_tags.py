51c7e981abbd4b6d63c3a9dd747f1946941646f2
Jeremy Keeshin <jkeesh@gmail.com>
2011-09-27 02:04:39 -0700

    proper styles are added to vote

--- application/qa/templatetags/vote_tags.py
+++ application/qa/templatetags/vote_tags.py
@@ -2,8 +2,17 @@ from django import template
 from django.conf import settings
 from django.template.defaultfilters import stringfilter
 register = template.Library()
-
+from qa.models import Vote
 
 @register.simple_tag
-def user_vote(obj, type):
-    return "worked"
+def user_vote(user, obj, kind, score):
+    try:
+        vote = Vote.objects.get(user=user, obj_id=obj.id, kind=kind)
+        if vote.score == 1 and score == 1:
+            return 'voted-up'
+        elif vote.score == -1 and score == -1:
+            return 'voted-down'
+        return ''
+    except Vote.DoesNotExist:
+        return ''
+        
